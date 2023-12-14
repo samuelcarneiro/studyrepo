87612ee93c453a2a3f297aa34d068c1c9f92906d
Samuel Carneiro <samuelcarneiro.al@gmail.com>
2023-12-14 09:11:50 -0300

    Exercício Classe Carro concluído

--- javascript/objetos-e-classes/03-praticando-com-objetos-e-classes/01-exercicio-classe-carro.js
+++ javascript/objetos-e-classes/03-praticando-com-objetos-e-classes/01-exercicio-classe-carro.js
@@ -8,17 +8,31 @@
  * As classes sempre começam com letra maiusculas
  */
 class Carro {
+    //características do carro
     marca;
     cor;
     gastoMedioPorKm;
 
-    constructor(marca, cor, gastoMedioPorKm) {
+    constructor(marca, modelo, cor, gastoMedioPorKm) {
         this.marca = marca;
+        this.modelo = modelo;
         this.cor = cor;
         this.gastoMedioPorKm = gastoMedioPorKm;
     }
+
+    //Método para Calcular o Gasto do Percurso
+    calcularGastoPercurso(distanciaEmKm, precoCombustivel) {
+        return (distanciaEmKm * this.gastoMedioPorKm) * precoCombustivel;
+    }
 }
 
-const uno = new Carro('fiat', 'branco', 1/12);
+//Instanciando um novo carro
+const uno = new Carro('Fiat', 'Uno', 'branco', 1/12);
+//Chamar o método calcularGastoPercurso com o Carro criado acima
+const gastoViagem = uno.calcularGastoPercurso(70, 5.0);
+console.log(`Custo da viagem com o ${uno.marca} ${uno.modelo} é de R$:`,gastoViagem.toFixed(2));
 
-console.log(uno);
+//Viagem com Palio
+const palio = new Carro('Fiat', 'Palio', 'prata', 1/10);
+const gastoViagem2 = palio.calcularGastoPercurso(70, 5.0);
+console.log(`Custo da viagem com o ${palio.marca} ${palio.modelo} é de R$:`,gastoViagem2.toFixed(2));
