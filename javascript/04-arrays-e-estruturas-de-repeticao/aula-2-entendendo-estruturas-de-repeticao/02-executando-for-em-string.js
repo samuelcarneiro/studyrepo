14971bb87fe8e5b97a73214dc5426f1a1e009994
Samuel Carneiro <samuelcarneiro.al@gmail.com>
2023-12-18 14:40:07 -0300

    Arquivo executando FOR em uma string

---
+++ javascript/04-arrays-e-estruturas-de-repeticao/aula-2-entendendo-estruturas-de-repeticao/02-executando-for-em-string.js
@@ -0,0 +1,10 @@
+/**
+ * Executando o FOR em uma String
+ */
+
+const nome = 'Samuel Carneiro';
+
+for (let i = 0; i < nome.length; i++) {
+    const letra = nome[i]; //Mostrará cara letra em uma linha
+    console.log(letra); 
+}
