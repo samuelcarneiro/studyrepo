2d0aaaa0299cd1d746fc91db4703025beef301e0
Samuel Carneiro <samuelcarneiro.al@gmail.com>
2023-12-18 13:27:28 -0300

    Entendendo listas/arrays

---
+++ javascript/04-arrays-e-estruturas-de-repeticao/aula-1-entendendo-arrays/03-entendendo-estruturas-de-repeticao.js
@@ -0,0 +1,17 @@
+/**
+ * ESTRUTURAS DE REPETIÇÃO
+ */
+
+const notas = [];
+
+notas.push(5);
+notas.push(7);
+notas.push(8);
+notas.push(2);
+notas.push(5);
+
+//notas.length; // Mostra o tamanho da lista
+
+const soma = notas[0] + notas[1] + notas[2] + notas[3] + notas[4];
+
+console.log(soma / notas.length); // Soma das notas dividida pelo tamanho da lista
