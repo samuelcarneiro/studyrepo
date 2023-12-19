eebdfc53161b084c703313eee8b54129c5a5d3e2
Samuel Carneiro <samuelcarneiro.al@gmail.com>
2023-12-19 17:05:45 -0300

    Exercício 2 em andamento...

---
+++ javascript/04-arrays-e-estruturas-de-repeticao/aula-3-praticando-estruturas-de-repeticao/exercicio-2-apenas-numeros-pares.js
@@ -0,0 +1,15 @@
+/**
+ * Exercício 2 - Imprimir apenas números pares 
+ * Crie um programa que seja capaz de percorrer uma lista de números e imprima cada número par encontrado
+ */
+
+const numeros = [];
+
+numeros.push(2);
+numeros.push(5);
+numeros.push(7);
+numeros.push(8);
+numeros.push(9);
+numeros.push(4);
+
+console.log(numeros);
