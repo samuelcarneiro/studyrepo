e1ee19a923ee08062b0f2eaab6bdadb83cfad381
Samuel Carneiro <samuelcarneiro.al@gmail.com>
2023-12-19 13:59:01 -0300

    Exercício tabuada com estrutura de repetição concluído!

---
+++ javascript/04-arrays-e-estruturas-de-repeticao/aula-3-praticando-estruturas-de-repeticao/exercicio-1-tabuada.js
@@ -0,0 +1,12 @@
+/**
+ * Exercício 01 - Tabuada
+ * 
+ * Crie um programa que dado um número, imprima sua tabuada
+ */
+
+let numero = 5;
+//console.log(numero + " x " + numero +" = "+ numero * numero);
+
+    for (let i = 1; i <= 10; i++) {
+        console.log(numero +" x "+ i +" = "+ numero * i);
+    }
