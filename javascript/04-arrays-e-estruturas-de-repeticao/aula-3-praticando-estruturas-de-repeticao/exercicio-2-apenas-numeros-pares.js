28d538524b4f91930a259e75f6c4f3af03e78b35
Samuel Carneiro <samuelcarneiro.al@gmail.com>
2023-12-20 12:58:11 -0300

    Exercício 2 concluído

--- javascript/04-arrays-e-estruturas-de-repeticao/aula-3-praticando-estruturas-de-repeticao/exercicio-2-apenas-numeros-pares.js
+++ javascript/04-arrays-e-estruturas-de-repeticao/aula-3-praticando-estruturas-de-repeticao/exercicio-2-apenas-numeros-pares.js
@@ -5,11 +5,27 @@
 
 const numeros = [];
 
+//Adicionando números ao array (lista) numeros
 numeros.push(2);
 numeros.push(5);
 numeros.push(7);
 numeros.push(8);
 numeros.push(9);
 numeros.push(4);
+numeros.push(12);
+numeros.push(37);
 
 console.log(numeros);
+/**
+ * Condição para que o programa imprima em tela somente os números pares
+ * Através do resto da divisão do número por 2 sendo igual a zero 
+ * "Se o resto da divisão do número por 2 for igual a zero, o número é par"
+ * Sendo par, o programa imprimirá o número
+ */ 
+for (let i = 0; i < numeros.length; i++) {
+    const numero = numeros[i];
+    if (numeros[i] % 2 == 0) {
+        console.log(numeros[i]);
+    }
+    
+}
