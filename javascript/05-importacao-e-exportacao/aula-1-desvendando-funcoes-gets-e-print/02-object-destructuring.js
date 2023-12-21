460dd4baf8280b2e658c1ce46bb9e7e49f544b53
Samuel Carneiro <samuelcarneiro.al@gmail.com>
2023-12-21 10:03:25 -0300

    Aula 01 - Concluída

--- javascript/05-importacao-e-exportacao/aula-1-desvendando-funcoes-gets-e-print/02-object-destructuring.js
+++ javascript/05-importacao-e-exportacao/aula-1-desvendando-funcoes-gets-e-print/02-object-destructuring.js
@@ -3,6 +3,7 @@ const { gets, print } = require("./01-criando-a-funcao-gets-e-print");
 
 print(gets()); // Retornará 10 - Valor retornado lá na função Gets
 
+
 //const pessoa = {
 //    nome: 'Samuel'
 //}
