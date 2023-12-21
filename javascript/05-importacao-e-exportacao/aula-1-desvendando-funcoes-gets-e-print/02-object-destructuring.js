9582ff462da6754f694b938cc18735d05e200aaf
Samuel Carneiro <samuelcarneiro.al@gmail.com>
2023-12-21 10:02:37 -0300

    Aula 01 - Conclída

--- javascript/05-importacao-e-exportacao/aula-1-desvendando-funcoes-gets-e-print/02-object-destructuring.js
+++ javascript/05-importacao-e-exportacao/aula-1-desvendando-funcoes-gets-e-print/02-object-destructuring.js
@@ -1,2 +1,11 @@
 // Importando as funções de outro arquivo (na mesma pasta)
-const funcoes = require("./01-criando-a-funcao-gets-e-print");
+const { gets, print } = require("./01-criando-a-funcao-gets-e-print");
+
+print(gets()); // Retornará 10 - Valor retornado lá na função Gets
+
+//const pessoa = {
+//    nome: 'Samuel'
+//}
+//const { nome } = pessoa;
+//const nome = pessoa.nome;
+// Ambos comandos são equivalentes
